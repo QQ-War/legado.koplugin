d3e7b6b6b47064d03efab288b26b14d3f8c29602
hizhongcai <hizhongcai@gmail.com>
2026-01-22 01:05:40 -0500

    Add image icon for manga proxy menu

--- legado.koplugin/Legado/Icons.lua
+++ legado.koplugin/Legado/Icons.lua
@@ -26,6 +26,7 @@ return {
     FA_BARS = "\u{F0C9}",
     FA_BELL = "\u{F0F3}",
     FA_CAMERA = "\u{F030}",
+    FA_IMAGE = "\u{F03E}",
     FA_CLOUD = "\u{F0C2}",
     FA_COG = "\u{F013}",
     FA_ENVELOPE = "\u{F0E0}",
